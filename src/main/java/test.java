c1381e570be9bfe3cd86a8b1435e7b1741064893
毛 <1296193245@qq.com>
2021-10-20 17:21:59 +0800

    Use_foreach_loop_to_traverse_the_Collection_collection

---
+++ src/main/java/test.java
@@ -0,0 +1,38 @@
+import java.util.HashSet;
+
+/**
+ * Project name(项目名称)：使用foreach循环遍历Collection集合
+ * Package(包名): PACKAGE_NAME
+ * Class(类名): test
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * Date(创建日期)： 2021/10/20
+ * Time(创建时间)： 17:18
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public class test
+{
+    public static void main(String[] args)
+    {
+        HashSet<String> p = new HashSet<>();
+        p.add("第一个字符串");
+        p.add("第二个字符串");
+        p.add("第三个字符串");
+        p.add("第四个字符串");
+        p.add("第五个字符串");
+        p.add("第六个字符串");
+        p.add("第七个字符串");
+        p.add("第八个字符串");
+        p.add("第九个字符串");
+        p.add("第十个字符串");
+        System.out.println(p.size());
+        for (String s : p)
+        {
+            System.out.println(s);
+        }
+        System.out.println(p.size());
+        p.forEach(System.out::println);
+    }
+}
